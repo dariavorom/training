81aa6010c5de418f2c0a9f304e3d16e160bd3d0f
dariavorom <dariavorom@gmail.com>
2021-02-15 17:33:12 +0300

    delete files

--- html3/task4/app.js
+++
@@ -1,8 +0,0 @@
-let val = document.querySelectorAll('.price');
-let outVal = document.querySelectorAll('.valOut')
-
-function out() {
-    for (let i = 0; i < val.length; i++) {
-        outVal[i].value = val[i].value;
-        }
-}
