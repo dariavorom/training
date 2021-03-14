81ee36c775de86dfa10e6891ea4da48814db13ea
dariavorom <dariavorom@gmail.com>
2021-03-14 19:05:39 +0300

    fix: ungrade functions

--- js1/app.js
+++ js1/app.js
@@ -20,7 +20,7 @@ let num3 = document.getElementById("task2_3");
 let resultTask2 = document.getElementById("answer2");
 document.getElementById("task2-result").addEventListener("click", countTask2);
 function countTask2 () {
-    resultTask2.value = (+num1.value + +num2.value + +num3.value)/3;
+    resultTask2.value = Math.round(((+num1.value + +num2.value + +num3.value)/3)*100)/100;
 }
 
 // Task3
@@ -30,8 +30,8 @@ function countTask3 () {
     let h = document.getElementById("task3_2");
     let v = document.getElementById("answer3_1");
     let s = document.getElementById("answer3_2");
-    v.value = Math.PI * + r.value * 2 * +h.value;
-    s.value = 2 * Math.PI * +r.value * (+r.value + +h.value);
+    v.value = Math.round((Math.PI * + r.value * 2 * +h.value)*100)/100;
+    s.value = Math.round((2 * Math.PI * +r.value * (+r.value + +h.value))*100)/100;
 }
 
 // Task4
@@ -148,14 +148,14 @@ rhombus();
 // Task7
 document.getElementById("task7-result").addEventListener("click", countTask7);
 function countTask7 () {
-   let arrSize = document.getElementById("task7_1").value;
-   let arr =  new Array(+arrSize).fill().map((d,el) => el = Math.floor(Math.random() * 100));
-   let arrShow = document.getElementById("answer7_1");
-   arrShow.value = arr;
-   //Наибольшее значение массива
-   let max = Math.max.apply(Math, arr);
-   let arrMax = document.getElementById("task7_2");
-   arrMax.value = max;
+    let arrSize = document.getElementById("task7_1").value;
+    let arr =  new Array(+arrSize).fill().map(el => el = Math.floor(Math.random() * 100));
+    let arrShow = document.getElementById("answer7_1");
+    arrShow.value = arr;
+    //Наибольшее значение массива
+    let max = Math.max.apply(Math, arr);
+    let arrMax = document.getElementById("task7_2");
+    arrMax.value = max;
     //Наименьшее значение массива
     let min = Math.min.apply(Math, arr);
     let arrMin = document.getElementById("task7_3");
@@ -167,7 +167,7 @@ function countTask7 () {
     //Среднее арифметическое всех элементов
     let average = arr.reduce((a, b) => (a + b)) / arr.length;
     let arrAverage = document.getElementById("task7_5");
-    arrAverage.value = average.toPrecision(3);
+    arrAverage.value = Math.round(average*100)/100;
     //Все нечетные значения
     let odd = arr.filter(el => el % 2 !== 0);
     let arrOdd = document.getElementById("task7_6");
@@ -182,18 +182,16 @@ arr[3] = [2,7,0,9,5];
 arr[4] = [2,3,6,-5,7];
 let arrSource = document.getElementById("arr");
 let arrNew = document.getElementById("newArr");
-arrSource.style.cssText = `
+function styles (el) {
+    el.style.cssText  = `
     border: 1px solid black;
     width: 150px;
     text-align: center;
     font-size: 22px;
-`;
-arrNew.style.cssText = `
-    border: 1px solid black;
-    width: 150px;
-    text-align: center;
-    font-size: 22px;
-`;
+`
+}
+styles(arrSource);
+styles(arrNew);
 for ( let i = 0; i < arr.length; i++) {
     let p = document.createElement("p")
     p.innerHTML = arr[i].toString().split(',').join(' ');
@@ -201,11 +199,12 @@ for ( let i = 0; i < arr.length; i++) {
 }
 for ( let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr[i].length; j++) {
-        if (i === j && arr[i][j] < 0) {
-            arr[i][j] = 0;
-
-        } else if (i === j && arr[i][j] > 0) {
-            arr[i][j] = 1;
+        if (i === j) {
+            if (arr[i][j] < 0) {
+                arr[i][j] = 0;
+            } else  {
+                arr[i][j] = 1;
+            }
         }
     }
     let p = document.createElement("p")
@@ -219,7 +218,7 @@ function countTask9 () {
     let vector = document.getElementById("task9_1").value.split(' ');
     let average = vector.reduce((a, b) => (+a + +b)) / vector.length;
     let vectorAverage = document.getElementById("task9_2");
-    vectorAverage.value = average;
+    vectorAverage.value = Math.round(average*100)/100;
     let arr = vector.filter(n => n > average);
     let quantity = document.getElementById("task9_3");
     quantity.value = arr.length;
